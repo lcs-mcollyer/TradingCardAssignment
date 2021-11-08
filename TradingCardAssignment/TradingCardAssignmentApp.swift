37b4ec9d25236408f3982a9e964e080a7ef8e440
matt collyer <mcollyer@lcs.on.ca>
2021-11-08 08:46:22 -0500

    Initial Commit

---
+++ TradingCardAssignment/TradingCardAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardAssignmentApp.swift
+//  TradingCardAssignment
+//
+//  Created by Matthew Collyer on 2021-11-08.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
